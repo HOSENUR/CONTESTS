3ad6cacfd1baa3a12d4cbe1d81c06adb3615de49
HOSENUR <nur08439@gmail.com>
2022-05-12 02:10:13 +0530

    Updated Template

---
+++ Template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FL(i,a,b) for(int i= a; i<= b; i++)
+
+string solve()
+{
+  return "ANSWER";
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout<<"A Basic C++ Template";
+}
