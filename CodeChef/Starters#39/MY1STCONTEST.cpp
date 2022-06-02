2dad52b590d3c4041d7fccd8b9247d047d01740e
HOSENUR <nur08439@gmail.com>
2022-06-02 21:52:52 +0530

    Solved Starters

---
+++ CodeChef/Starters#39/MY1STCONTEST.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FL(i,a,b) for(int i= a; i<= b; i++)
+
+void solve()
+{
+  int n,a,b;
+  cin>>n>>a>>b;
+
+  cout << n-a <<" "<< n-a-b;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  solve();
+}
